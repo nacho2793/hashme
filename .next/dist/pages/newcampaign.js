471eb401a5f7ed230ff446397ef057c3422ff3a8
Nacho <0163473@up.edu.mx>
2018-01-29 11:55:29 -0600

    next

---
+++ .next/dist/pages/newcampaign.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = 'D:\\Development\\Crypto\\hashme\\pages\\newcampaign.js?entry';
+
+exports.default = function () {
+	return _react2.default.createElement('h1', {
+		__source: {
+			fileName: _jsxFileName,
+			lineNumber: 5
+		}
+	}, 'This is the new campaign page!!!');
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzXFxuZXdjYW1wYWlnbi5qcyJdLCJuYW1lcyI6WyJSZWFjdCJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTyxBQUVQOzs7Ozs7OztrQkFBZSxZQUFNLEFBQ3BCO3dCQUNDLGNBQUE7O2FBQUE7ZUFBQTtBQUFBO0FBQUEsRUFBQSxFQURELEFBQ0MsQUFFRDtBQUpEIiwiZmlsZSI6Im5ld2NhbXBhaWduLmpzP2VudHJ5Iiwic291cmNlUm9vdCI6IkQ6L0RldmVsb3BtZW50L0NyeXB0by9oYXNobWUifQ==
--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -57,8 +57,42 @@ describe('Campaigns', () => {
 				value: '5',
 				from: accounts[1]
 			});
+			assert(false);
 		} catch(err){
-
+			assert(err);
 		} 
 	});
+	it('allows a manager to make a payment request', async () => {
+		await campaign.methods
+			.createRequest('buy batteries', '100', accounts[1])
+			.send({
+				from: accounts[0],
+				gas: '1000000'
+			});
+		const request = await campaign.methods.requests(0).call();
+		assert.equal('buy batteries', request.description);
+	});
+
+	it('processes request', async () => {
+		await campaign.methods.contribute().send({
+			from: accounts[0],
+			value: web3.utils.toWei('10', 'ether')
+		});
+		await campaign.methods
+			.createRequest('A', web3.utils.toWei('5', 'ether'), accounts[1])
+			.send({from: accounts[0], gas: '1000000'});
+		await campaign.methods.approveRequest(0).send({
+			from: accounts[0],
+			gas: '1000000'
+		});
+		await campaign.methods.finalizeRequest(0).send({
+			from: accounts[0],
+			gas: '1000000'
+		});
+
+		let balance = await web3.eth.getBalance(accounts[1]);
+		balance = web3.utils.fromWei(balance, 'ether');
+		balance = parseFloat(balance);
+		assert(balance > 104)
+	})
 });
